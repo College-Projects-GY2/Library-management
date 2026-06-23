f9fc8dbcd48b5a6ea50c9c668114cb4f3be91923
yonatanfarhi20 <yonatanfarhi1@gmail.com~>
2026-06-23 11:26:06 +0300

    Infrastructure(Book, Member, Admin, Library)

---
+++ Member.java
@@ -0,0 +1,8 @@
+public class Member {
+    private String id;
+    private String username;
+    private String password;
+    private boolean isPremium;
+    private book[] borrowedBooks;
+    private  int borrowedCount;
+}
