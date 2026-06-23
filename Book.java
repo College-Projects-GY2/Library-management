ce9c07224ad374cbb49ac4b03008ea21383735b9
ariel <ariel.kaplan011@gmail.com>
2026-06-23 18:29:34 +0300

    create functions Book, getTitle, getAuthor, getGenre

--- Book.java
+++ Book.java
@@ -3,4 +3,16 @@ public class Book{
     private String author;
     private String genre;
     private boolean isBorrowed;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
 }
