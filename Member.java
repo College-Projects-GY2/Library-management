d14de201e1e98af744c166369ac48c522e6d1e9d
ariel <ariel.kaplan011@gmail.com>
2026-06-25 22:34:24 +0300

    create functions Member, getId, getUsername, getPassword, isPremium, getBorrowedBooks

--- Member.java
+++ Member.java
@@ -5,4 +5,39 @@ public class Member {
     private boolean isPremium;
     private []book borrowedBooks;
     private int borrowedCount;
+
+
+    public Member(boolean isPremium) {
+        this.isPremium = isPremium;
+
+        if (isPremium == true) {
+            this.borrowedBooks = new Book[10];
+        } else {
+            this.borrowedBooks = new Book[3];
+        }
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean isPremium() {
+        return this.isPremium;
+    }
+
+    public book getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public Book[] getBorrowedBooks() {
+        return this.borrowedBooks;
+    }
 }
