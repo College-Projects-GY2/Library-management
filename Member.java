e07e76fe7bbbdb22a7e37f79ffbfc5da31436065
ariel <ariel.kaplan011@gmail.com>
2026-06-30 19:27:23 +0300

    Create Member class with basic attributes and borrow/return functions

--- Member.java
+++ Member.java
@@ -3,14 +3,15 @@ public class Member {
     private String username;
     private String password;
     private boolean isPremium;
-    private []book borrowedBooks;
+    private Book[] borrowedBooks;
     private int borrowedCount;
 
 
     public Member(boolean isPremium) {
         this.isPremium = isPremium;
+        this.borrowedCount = 0;
 
-        if (isPremium == true) {
+        if (isPremium) {
             this.borrowedBooks = new Book[10];
         } else {
             this.borrowedBooks = new Book[3];
@@ -33,11 +34,37 @@ public class Member {
         return this.isPremium;
     }
 
-    public book getBorrowedBooks() {
-        return borrowedBooks;
-    }
-
     public Book[] getBorrowedBooks() {
         return this.borrowedBooks;
+    }
+
+    public int getBorrowedCount() {
+        return this.borrowedCount;
+    }
+
+    public boolean borrowBook(Book book) {
+        if (this.borrowedCount >= this.borrowedBooks.length) {
+            return false;
+        }
+        this.borrowedBooks[this.borrowedCount] = book;
+        this.borrowedCount++;
+        return true;
+    }
+
+    public boolean returnBook(Book book) {
+        for (int i = 0; i < this.borrowedCount; i++) {
+            if (this.borrowedBooks[i].equals(book)) {
+
+                for (int j = i; j < this.borrowedCount - 1; j++) {
+                    this.borrowedBooks[j] = this.borrowedBooks[j + 1];
+                }
+
+                this.borrowedBooks[this.borrowedCount - 1] = null;
+                this.borrowedCount--;
+
+                return true;
+            }
+        }
+        return false;
     }
 }
