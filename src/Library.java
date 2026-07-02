3c4dbc789da9438a18d7c669281ce4fcd46027ce
yairr <yairrr20000@gmail.com>
2026-07-02 16:14:50 +0300

    create function addBook

--- src/Library.java
+++ src/Library.java
@@ -47,5 +47,14 @@ public class Library {
 
         return null;
     }
+   public boolean addBook(String title, String author, String genre){
+       if (this.bookCount >= 100) {
+           return false;
+       }
+
+       this.book[this.bookCount] = new Book(title, author, genre);
+       this.bookCount++;
+       return true;
+    }
 
 }
