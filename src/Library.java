9e6b4be6cf8c24dfd2c4d765919a9d265aeb13fd
yairr <yairrr20000@gmail.com>
2026-07-02 15:36:13 +0300

    create functions of Library

--- src/Library.java
+++ src/Library.java
@@ -4,4 +4,20 @@ public class Library {
     private Member[] members;
     private int memberCount;
     private Admin admin;
+    public Library() {
+        this.book = new Book[100];
+        this.bookCount = 0;
+        this.members = new Member[100];
+        this.memberCount = 0;
+        this.admin = new Admin("admin", "admin123");
+        creatingTenBooks();
+    }
+
+    private void creatingTenBooks() {
+        for (int i = 0; i < 10; i++) {
+            this.book[this.bookCount] = new Book("Book " + (i + 1), "Author " + (i + 1), "General");
+            this.bookCount++;
+        }
+    }
+
 }
